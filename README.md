8a749f37de8090a9634a95e558a63c3bfaef3bf2
formicant <formicant@gmail.com>
2023-03-04 13:00:03 +0700

    PWM, encoder input

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # LED-PWM
-Controls LED brightness
+
+A program for `ATTiny2313` for controlling LED brightness using a rotary encoder.
+
+
+## Pins
+
+``` 
+                           ATTiny 2313
+                           ┌───┐ ┌───┐
+            (RESET/dW) PA2─┤1  ╰─╯ 20├─VCC
+                 (RXD) PD0─┤2      19├─PB7 (UCSK/SCL/PCINT7)
+                 (TXD) PD1─┤3      18├─PB6 (MISO/DO/PCINT6)
+               (XTAL2) PA1─┤4      17├─PB5 (MOSI/DI/SDA/PCINT5)
+               (XTAL1) PA0─┤5      16├─PB4 (OC1B/PCINT4)
+Encoder CLK  →  (INT0) PD2─┤6      15├─PB3 (OC1A/PCINT3)
+Encoder DT   →  (INT1) PD3─┤7      14├─PB2 (OC0A/PCINT2)
+                  (T0) PD4─┤8      13├─PB1 (AIN1/PCINT1)  →  LED
+             (OC0B/T1) PD5─┤9      12├─PB0 (AIN0/PCINT0)
+Encoder GND  →         GND─┤10     11├─PD6 (ICP)
+                           └─────────┘
+```
+
+## Opeartion
+
+16-bit PWM channel 0 is used in Fast PWM 9-bit mode.
+
---
+++ pwm.asm
@@ -0,0 +1,213 @@
+; ATTiny2313 constants
+.include "tn2313def.inc"
+
+; TCCR1A modes
+.equ pwm_clear  = 1 << COM1A1
+.equ pwm_8bit   = 1 << WGM10
+.equ pwm_9bit   = 1 << WGM11
+.equ pwm_10bit  = (1 << WGM10) | (1 << WGM11)
+.equ pwm_icr_a  = 1 << PWM11
+; TCCR1B modes
+.equ pwm_icr_b  = 1 << WGM13
+.equ pwm_fast   = 1 << WGM12
+.equ pwm_scale1 = 1 << CS10
+
+
+; Port B output pin
+.equ led_pwm  = (1 << PORTB3)
+
+; Port D input pins
+.equ encoder_clk  = (1 << PIND2)
+.equ encoder_dt   = (1 << PIND3)
+.equ encoder_mask = encoder_clk | encoder_dt
+
+; Constants
+; .equ pwm_period = 512
+; (custom PWM periods are not supported by VMLAB emulator)
+.equ bounce_period = 1000  ; microseconds
+
+; Register aliases
+.def zero  = r16
+.def tmp   = r17
+.def input = r18
+.def level = r19
+
+
+; Interrupt vector table
+; No interrupts are used
+    rjmp reset ; RES  Reset
+    reti       ; INT0 External Interrupt Request 0
+    reti       ; INT1 External Interrupt Request 1
+    reti       ; ICP1 Timer/Counter1 Capture Event
+    reti       ; OC1A Timer/Counter1 Compare Match A
+    reti       ; OVF1 Timer/Counter1 Overflow
+    reti       ; OVF0 Timer/Counter0 Overflow
+    reti       ; URXC USART, Rx Complete
+    reti       ; UDRE USART Data Register Empty
+    reti       ; UTXC USART, Tx Complete
+    reti       ; ACI  Analog Comparator
+    reti       ; PCI
+    reti       ; OC1B
+    reti       ; OC0A
+    reti       ; OC0B
+    reti       ; USI_START USI Start Condition
+    reti       ; USI_OVF   USI Overflow
+    reti       ; ERDY
+    reti       ; WDT  Watchdog Timer Overflow
+
+
+; PWM level table
+; quadratic curve, 32 levels
+levels:
+    .dw  0     ; off
+    .dw  0     ; minimum
+    .dw  1
+    .dw  2
+    .dw  5
+    .dw  9
+    .dw  14
+    .dw  20
+    .dw  28
+    .dw  36
+    .dw  46
+    .dw  57
+    .dw  69
+    .dw  82
+    .dw  96
+    .dw  111
+    .dw  128
+    .dw  145
+    .dw  164
+    .dw  184
+    .dw  205
+    .dw  227
+    .dw  250
+    .dw  275
+    .dw  300
+    .dw  327
+    .dw  355
+    .dw  384
+    .dw  414
+    .dw  445
+    .dw  478
+    .dw  511   ; maximum
+levels_end:
+.equ level_count = levels_end - levels
+
+
+; Program starts here after reset
+reset:
+    ; disable interrupts
+    cli
+
+    ; initialize stack
+    ldi  tmp, low(RAMEND)
+    out  SPL, tmp
+
+    ; initialize input ports with pull-up resistors
+    ldi  tmp, encoder_mask
+    out  PORTD, tmp
+
+    ; initialize output port
+    ldi  tmp, led_pwm
+    out  DDRB, tmp
+
+    ; initialize PWM timer
+
+    ; (custom PWM periods are not supported by VMLAB emulator.
+    ; New ATTiny2313A models should support this)
+    ; ldi  XL, low (pwm_period - 1)
+    ; ldi  XH, high(pwm_period - 1)
+    ; out  ICR1H, XH
+    ; out  ICR1L, XL
+
+    ; fast PWM mode, no clock prescaling
+    ldi  tmp, pwm_fast | pwm_scale1
+    out  TCCR1B, tmp
+    ; set on start, clear on match, 9-bit PWM
+    ldi  tmp, pwm_clear | pwm_9bit
+    out  TCCR1A, tmp
+
+    ; initialize register values
+    clr  zero
+    clr  level
+
+set_level:
+    ; get address in the level table
+    ; addr = 2 * (levels + level)
+    ldi  ZL, low (2 * levels)
+    ldi  ZH, high(2 * levels)
+    add  ZL, level
+    adc  ZH, zero
+    add  ZL, level
+    adc  ZH, zero
+    ; get level value from the table
+    lpm  XL, Z+
+    lpm  XH, Z
+    ; set PWM value
+    out  OCR1AH, XH
+    out  OCR1AL, XL
+
+input_loop:
+    ; wait until both encoder pins are active (low signal) for bounce_period
+    ldi  XL, low (bounce_period)
+    ldi  XH, high(bounce_period)
+
+encoder_turning_debounce:
+    in   input, PIND
+    andi input, encoder_mask
+    brne input_loop
+    sbiw XL, 1
+    nop  ; debouncing loop takes 8 cycles = 1 us
+    brne encoder_turning_debounce
+
+encoder_turning:
+    ; wait until one of the encoder pins is inactive (high signal) for bounce_period
+    ldi  XL, low (bounce_period)
+    ldi  XH, high(bounce_period)
+
+encoder_turned_debounce:
+    in   input, PIND
+    andi input, encoder_mask
+    breq encoder_turning
+    sbiw XL, 1
+    nop  ; debouncing loop takes 8 cycles = 1 us
+    brne encoder_turned_debounce
+
+encoder_turned:
+    ; determine which direction the encoder is turned
+    cpi  input, encoder_dt
+    breq clockwise
+    cpi  input, encoder_clk
+    brne input_loop
+
+counter_clockwise:
+    ; decrement level if possible
+    tst  level
+    breq input_loop
+    dec  level
+    ; if level is 0, turn off PWM
+    brne set_level
+
+turn_off:
+    clr  tmp
+    out  TCCR1A, tmp
+    out  PORTB, tmp
+    rjmp input_loop
+
+clockwise:
+    ; if level was 0, turn on PWM
+    tst  level
+    brne increment_level
+
+turn_on:
+    ldi  tmp, pwm_clear | pwm_9bit
+    out  TCCR1A, tmp
+
+increment_level:
+    ; increment level if possible
+    cpi  level, level_count - 1
+    breq input_loop
+    inc  level
+    rjmp set_level
+
